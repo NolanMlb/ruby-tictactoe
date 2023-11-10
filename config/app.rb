a33b01ee4f38baa0be4cadbf47224a3522274e42
NolanMlb <nolanmlb@outlook.fr>
2023-11-10 23:03:22 +0100

    go dodo

--- config/app.rb
+++ config/app.rb
@@ -8,25 +8,25 @@ connection = PG.connect(dbname: 'tictactoe', user: 'postgres', password: 'postgr
 connection.exec("CREATE TABLE IF NOT EXISTS players (
                   id VARCHAR PRIMARY KEY,
                   name VARCHAR NOT NULL
-              );
-              CREATE TABLE IF NOT EXISTS games (
-                  id VARCHAR PRIMARY KEY,
-                  player_x_id VARCHAR NOT NULL,
-                  player_o_id VARCHAR NOT NULL,
-                  winner VARCHAR NULL,
-                  status VARCHAR NOT NULL CHECK (status IN ('ongoing', 'finished')),
+                );
+                CREATE TABLE IF NOT EXISTS games (
+                    id VARCHAR PRIMARY KEY,
+                    player_x_id VARCHAR NOT NULL,
+                    player_o_id VARCHAR NOT NULL,
+                    winner VARCHAR NULL,
+                    status VARCHAR NOT NULL CHECK (status IN ('ongoing', 'finished')),
 
-                  FOREIGN KEY (player_x_id) REFERENCES players(id),
-                  FOREIGN KEY (player_o_id) REFERENCES players(id)
-              );
-              CREATE TABLE IF NOT EXISTS moves (
-                  game_id VARCHAR NOT NULL,
-                  x_pos INT NOT NULL,
-                  y_pos INT NOT NULL,
-                  movement_index INT NOT NULL,
+                    FOREIGN KEY (player_x_id) REFERENCES players(id),
+                    FOREIGN KEY (player_o_id) REFERENCES players(id)
+                );
+                CREATE TABLE IF NOT EXISTS moves (
+                    game_id VARCHAR NOT NULL,
+                    x_pos INT NOT NULL,
+                    y_pos INT NOT NULL,
+                    movement_index INT NOT NULL,
 
-                  FOREIGN KEY (game_id) REFERENCES games(id)
-              );")
+                    FOREIGN KEY (game_id) REFERENCES games(id)
+                );")
 result = connection.exec("SELECT * FROM games;")
 print(result)
 
